5e5fb7a883ec9ef06936768684babcec3684b9dd
ismaDiaz <bd.isma.diaz@gmail.com>
2018-05-04 18:48:01 +0200

    P5_Quiz

---
+++ core/p5core/P5_Quiz/models/index.js
@@ -0,0 +1,33 @@
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize("sqlite:quizzes.sqlite", {logging: false});
+
+sequelize.define('quiz', {
+    question: {
+        type: Sequelize.STRING,
+        unique: {nsg: "Ya no existe esta pregunta"},
+        validate: {notEmpty: {nsg: "La respuesta no puede estar vacía"}}
+    },
+    answer: {
+        type: Sequelize.STRING,
+        validate: {notEmpty: {nsg: "La respuesta no puede estar vacía"}}
+    }
+});
+
+sequelize.sync()
+.then(() => sequelize.models.quiz.count())
+.then(count => {
+    if (!count) {
+        return sequelize.models.quiz.bulkCreate([
+            { question: "Capital de Italia", answer: "Roma"},
+            { question: "Capital de Francia", answer: "Par�s"},
+            { question: "Capital de Espa�a", answer: "Madrid"},
+            { question: "Capital de Portugal", answer: "Lisboa"}
+        ]);
+    }
+})
+.catch(error => {
+    console.log(error);
+});
+
+module.exports = sequelize;
--- core/p5core/P5_Quiz/routes/index.js
+++ core/p5core/P5_Quiz/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+//var quizzes = require('./models/index')
 
 
 /* GET home page. */
@@ -7,5 +8,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 /* GET Credits. */
+router.get('/credits', function(req, res, next) {
+  res.render('credits', { title: 'Credits' });
+});
+/* GET Quizzes. */
+router.get('/credits', function(req, res, next) {
+  var q = quizzes.findAll();
+  res.render('quizzes', { title: 'Quizzes' });
+});
 
 module.exports = router;
