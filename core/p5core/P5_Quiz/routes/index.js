f995e79227d9358b93d371b0c7c3c54c44f87b80
ismaDiaz <bd.isma.diaz@gmail.com>
2018-05-04 18:06:36 +0200

    P5_Quiz

--- core/p5core/P5_Quiz/routes/index.js
+++ core/p5core/P5_Quiz/routes/index.js
@@ -1,14 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var routerCredits = express.Router();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 /* GET Credits. */
-router.get('/credits', function(req, res, next) {
-  res.send('Responds to: /credits');
-});
+
 module.exports = router;
-module.exports = routerCredits;
