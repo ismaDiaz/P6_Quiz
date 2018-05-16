88072f0c1ebd9d1b87d4c38bbf79b707f07afdea
Arturo <a.domero@alumnos.upm.es>
2018-05-16 04:04:59 +0100

    practica6

--- routes/index.js
+++ routes/index.js
@@ -30,5 +30,9 @@ router.delete('/quizzes/:quizId(\\d+)',    quizController.destroy);
 router.get('/quizzes/:quizId(\\d+)/play',  quizController.play);
 router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
 
+router.get('/quizzes/random_play',         quizController.randomplay)
+router.get('/quizzes/random_nomore',       quizController.randomplay)
+router.get('/quizzes/random_result/:quizId(\\d+)',       quizController.randomcheck)
+
 
 module.exports = router;
