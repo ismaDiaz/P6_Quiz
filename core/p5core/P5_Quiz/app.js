9ba71821629b82e0ea19b5f971bb554254195cd9
ismaDiaz <bd.isma.diaz@gmail.com>
2018-05-19 18:43:39 +0200

    p5_quiz

--- core/p5core/P5_Quiz/app.js
+++ core/p5core/P5_Quiz/app.js
@@ -3,8 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-
 const http = require('http').createServer(app);
 
 var indexRouter = require('./routes/index');
